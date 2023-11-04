5d6e922f735e7c236af79d708209ff1ed1fae713
Leonardo Siu <leonardoy.siu@gmail.com>
2023-11-04 14:20:32 -0700

    basic layout completed

---
+++ script.js
@@ -0,0 +1,13 @@
+function getInitialDestination() 
+{
+    // saves users input into intial and destination variables
+    var nameInitial = document.getElementById("initial");
+    var nameDestination = document.getElementById("destination");
+
+    print(nameInitial, nameDestination)
+ 
+}
+
+
+// Save the locations as well as the user's selected filters (price, time, etc.) and save those into a variables.
+// Then, save those variables to a maybe a file or simply return it to be parsed by back-end.
